3893ec761ddd4156373fcd36ec9b3f71a95e123a
uhyo <uhyo@uhy.ooo>
2021-05-09 00:44:21 +0900

    feat(akitainu): support passing config file path to CLI

--- packages/akitainu/src/cli/index.ts
+++ packages/akitainu/src/cli/index.ts
@@ -2,6 +2,7 @@
 
 import { cosmiconfig } from "cosmiconfig";
 import path from "path";
+import yargs from "yargs";
 import { Checker } from "../checker/index.js";
 import { runReporters, runRules } from "../core/index.js";
 import { Filter } from "../filter/index.js";
@@ -13,19 +14,30 @@ import { nullSource } from "../source/nullSource.js";
 import { CliConfig, getDefaultConfig } from "./config.js";
 import { resolvePackage } from "./resolvePackage.js";
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+yargs(process.argv.slice(2))
+  .command(
+    "$0 [configfile]",
+    "Run akitainu",
+    (yargs) => {
+      return yargs.positional("configfile", {
+        type: "string",
+      });
+    },
+    (argv) => {
+      const configFile = argv.configfile;
+      main(configFile).catch((err) => {
+        console.error(err);
+        process.exit(1);
+      });
+    }
+  )
+  .help().argv;
 
-async function main() {
-  const explorer = cosmiconfig("akitainu");
-  const searchResult = await explorer.search();
+async function main(configFile: string | undefined) {
+  const { configDir, config: rawConfig } = await loadConfig(configFile);
   const config: CliConfig = {
-    ...getDefaultConfig(
-      searchResult ? path.dirname(searchResult.filepath) : process.cwd()
-    ),
-    ...(searchResult?.config ?? {}),
+    ...getDefaultConfig(configDir),
+    ...rawConfig,
   };
   const [rules, reporters] = await Promise.all([
     getRules(config),
@@ -45,6 +57,20 @@ async function main() {
     // If there was an error, exit with non-zero exit code
     process.exit(64);
   }
+}
+
+async function loadConfig(configFile: string | undefined) {
+  const explorer = cosmiconfig("akitainu");
+  const searchResult =
+    configFile === undefined
+      ? await explorer.search()
+      : await explorer.load(configFile);
+  return {
+    configDir: searchResult
+      ? path.dirname(searchResult.filepath)
+      : process.cwd(),
+    config: searchResult?.config ?? {},
+  };
 }
 
 function getRules(config: CliConfig): Promise<Rule[]> {
